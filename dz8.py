fc3e918039cad23cc1c7e4e614d9052b21d66890
Chiefpro <chiefpro@yandex.ru>
2023-07-21 21:32:35 +0300

    dz8.2

--- dz8.py
+++ dz8.py
@@ -12,13 +12,10 @@ def read_d():
     print()
     with open(PAHT, "r", encoding="UTF-8") as file:
         t = file.readlines()
-    new_book = {}
-    for i, contact in enumerate(t, 1):
-        contact = contact.strip().split(";")    
-        new_book[i] = {"name" : contact[0], "phone" : contact[1], "comment" : contact[2]} 
-    
-    for i in new_book:
-        print(new_book.get(i).get("name"),"\t", new_book.get(i).get("phone"),"\t", new_book.get(i).get("comment"))
+
+    for i in t:
+        i = i.strip().split(";")
+        print(f"{i[0]}\t{i[1]}\t{i[2]}")
     print()
     input("Для продолжения нажми Enter ")
     clear()
@@ -36,30 +33,82 @@ def write_d():
 
 
 def find_d():
-    print("ff")
+    print()
+    with open(PAHT, "r", encoding="UTF-8") as file:
+        t = file.readlines()
+    seach_d = input("Введи имя искомого человека => ")
+    dt = True
+    for i in range(len(t)):
+        t_lits = t[i].strip().split(";")
+        if t_lits[0] == seach_d:
+            print(f"{t_lits[0]}\t{t_lits[1]}\t{t_lits[2]}")
+            dt = False
+    if dt:
+        print("Такого человека в списке нету")
+    print()
+    input("Для продолжения нажми Enter ")
+    clear()
 
 
 def remove_d():
-    print("ff")
+    print()
+    with open(PAHT, "r", encoding="UTF-8") as file:
+        t = file.readlines()
+    seach_d = input("Введи имя человека для удаления => ")
+    for i in range(len(t)):
+        t_lits = t[i].strip().split(";")
+        if t_lits[0] == seach_d:
+            print(f"{t_lits[0]}\t{t_lits[1]}\t{t_lits[2]}")
+            ti = i
+    while True:
+        question = input("Точно удалить?\tДА = 1\tНЕТ = 0 \t=>\t")
+        if question.isdigit():
+            question = int(question)
+            if question == 1 or question == 0:
+                break
+        else:
+            print("Попробуйте ещё раз")
+    
+    if question == 1:
+        t.pop(ti)
+        tr = "".join(t)
+        with open(PAHT, "w", encoding="UTF-8") as file:
+            file.write(tr)
+    print()
+    input("Для продолжения нажми Enter ")
+    clear()
 
 def edit_d():
     print()
     with open(PAHT, "r", encoding="UTF-8") as file:
         t = file.readlines()
-    new_book = {}
-    for i, contact in enumerate(t, 1):
-        contact = contact.strip().split(";")    
-        new_book[i] = {"name" : contact[0], "phone" : contact[1], "comment" : contact[2]} 
+    seach_d = input("Введи имя человека для редактирования => ")
+    for i in range(len(t)):
+        t_lits = t[i].strip().split(";")
+        if t_lits[0] == seach_d:
+            print(f"{t_lits[0]}\t{t_lits[1]}\t{t_lits[2]}")
+            ti = i
+    while True:
+        question = input("Точно удалить?\tДА = 1\tНЕТ = 0 \t=>\t")
+        if question.isdigit():
+            question = int(question)
+            if question == 1 or question == 0:
+                break
+        else:
+            print("Попробуйте ещё раз")
     
-    for i in new_book:
-        print(new_book.get(i).get("name"),"\t", new_book.get(i).get("phone"),"\t", new_book.get(i).get("comment"))
+    if question == 1:
+        t.pop(ti)
+        tr = "".join(t)
+        with open(PAHT, "w", encoding="UTF-8") as file:
+            file.write(tr)
     print()
     input("Для продолжения нажми Enter ")
     clear()
 
 while True:
     while True:
-        print("Найти контакт = 1\tДобавить контакт = 2\tУдалить контакт = 3\tРедактировать контакт = 4\tПоказать весь справочник = 5\tВыход = 8")
+        print("Найти контакт = 1\tДобавить контакт = 2\tУдалить контакт = 3\tРедактировать контакт = 4\tПоказать весь справочник = 5\tВыход = 0")
         vibor = input("Выберите действие => ")
         if vibor.isdigit():
             vibor = int(vibor)
@@ -76,7 +125,7 @@ while True:
         edit_d()
     elif vibor == 5:
         read_d()
-    elif vibor == 8:
+    elif vibor == 0:
         break
     else:
         clear()
