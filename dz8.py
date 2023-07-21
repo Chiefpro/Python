ece916a74b31caf3014617fa05b922e702fb8d87
Chiefpro <chiefpro@yandex.ru>
2023-07-21 20:20:31 +0300

    dz8.1

--- dz8.py
+++ dz8.py
@@ -1,18 +1,39 @@
 import json
-PAHT = "Python/sample_data/directory.txt"
+import os
+clear = lambda: os.system('cls')
+clear()
+
+PAHT = "sample_data/directory.txt"
+clear()
+
 
 
 def read_d():
-    print("ff")
-    # with open(PAHT, "w", encoding="UTF-8") as file:
+    print()
+    with open(PAHT, "r", encoding="UTF-8") as file:
+        t = file.readlines()
+    new_book = {}
+    for i, contact in enumerate(t, 1):
+        contact = contact.strip().split(";")    
+        new_book[i] = {"name" : contact[0], "phone" : contact[1], "comment" : contact[2]} 
+    
+    for i in new_book:
+        print(new_book.get(i).get("name"),"\t", new_book.get(i).get("phone"),"\t", new_book.get(i).get("comment"))
+    print()
+    input("Для продолжения нажми Enter ")
+    clear()
 
 
 def write_d():
-    print("ff")
-    # with open(PAHT, "w", encoding="UTF-8") as file:
-    #     name = input("Введите имя контакта => ")
-    #     tel = input("Введите телефон контакта => ")
-    #     comment = input("Введите комментарий для контакта => ")
+    print()
+    name = input("Введите имя контакта => ")
+    tel = input("Введите телефон контакта => ")
+    comment = input("Введите комментарий для контакта => ")
+    temp_kontakt = name+";"+tel+";"+comment
+    print(temp_kontakt)
+    with open(PAHT, "a", encoding="UTF-8") as file:
+        print(temp_kontakt, file=file)
+
 
 def find_d():
     print("ff")
@@ -21,10 +42,30 @@ def find_d():
 def remove_d():
     print("ff")
 
+def edit_d():
+    print()
+    with open(PAHT, "r", encoding="UTF-8") as file:
+        t = file.readlines()
+    new_book = {}
+    for i, contact in enumerate(t, 1):
+        contact = contact.strip().split(";")    
+        new_book[i] = {"name" : contact[0], "phone" : contact[1], "comment" : contact[2]} 
+    
+    for i in new_book:
+        print(new_book.get(i).get("name"),"\t", new_book.get(i).get("phone"),"\t", new_book.get(i).get("comment"))
+    print()
+    input("Для продолжения нажми Enter ")
+    clear()
+
 while True:
-    print("Найти контакт = 1\tДобавить контакт = 2\tУдалить контакт = 3\tВыход = 4")
-    vibor = input("Выберите действие => ")
-    vibor = int(vibor)
+    while True:
+        print("Найти контакт = 1\tДобавить контакт = 2\tУдалить контакт = 3\tРедактировать контакт = 4\tПоказать весь справочник = 5\tВыход = 8")
+        vibor = input("Выберите действие => ")
+        if vibor.isdigit():
+            vibor = int(vibor)
+            break
+        else:
+            clear()
     if vibor == 1:
         find_d()
     elif vibor == 2:
@@ -32,6 +73,12 @@ while True:
     elif vibor == 3:
         remove_d()
     elif vibor == 4:
+        edit_d()
+    elif vibor == 5:
+        read_d()
+    elif vibor == 8:
         break
+    else:
+        clear()
 
 
